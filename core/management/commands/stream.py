66a5a0133708368a7fe1b35b01dade547c85657f
Nate <nelliott.ca@gmail.com>
2017-12-14 08:40:29 -0500

    streaming from dir

--- core/management/commands/stream.py
+++ core/management/commands/stream.py
@@ -1,21 +1,24 @@
 from django.core.management import BaseCommand
 from channels import Group
-from random import randrange
 from time import sleep
+import os
 
 
-# The class must be named Command, and subclass BaseCommand
+
 class Command(BaseCommand):
 
     help = "Stream all the numbers."
 
-
-    # A command must define handle()
     def handle(self, *args, **options):
-        while True:
-            params = {
-                'text': str(randrange(999, 10000))
-            }
 
-            Group('system').send(params)
-            sleep(1.5)
+        path = '/home/nate/homesite/bin/streams'
+
+        while True:
+            for stream in os.listdir(path):
+                with open(os.path.join(path,stream)) as file:
+                    for ch in file.read():
+                        self.transmit(hex(ord(ch))[2:])
+
+    def transmit(self,text,pause=1.25):
+        Group('system').send({'text': text})
+        sleep(pause)
